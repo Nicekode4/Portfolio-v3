7792a84983b12282328b65f86b9559649cb0034e
Philip Thomsen <philip-th@live.dk>
2023-01-13 10:51:37 +0100

    main content area update

--- portfolio/src/Components/Main/Content.jsx
+++ portfolio/src/Components/Main/Content.jsx
@@ -7,23 +7,25 @@ function Content() {
         <h1>Hejsa! Jeg er PH</h1>
         <h5>Selvudlært webudwikler med <span>7 års</span> erfaring</h5>
 
-        <div>
+        <div className='buttons'>
             <button type='button'>Indhent tilbud</button>
-            <button type='button'>Projekter</button>        
+            <button type='button'>Projekter <span>⭷</span></button>        
         </div>
         <div className='mainGrid'>
             <div>
                 <p>+84</p>
                 <p>Clients</p>
             </div>
+            <div></div>
             <div>
                 <p>+84</p>
                 <p>Clients</p>
             </div>
-            <div>
+            <div className='contact'>
                 <p>Kontakt info:</p>
-                <p><a href="mailto:philip@kode4.dk">📧</a></p>
-                <p><a href="tel:+4522966016">☎️</a></p>
+                <span>                <p><a href="mailto:philip@kode4.dk">📧</a></p>
+                <p><a href="tel:+4522966016">☎️</a></p></span>
+
             </div>
         </div>
         <Footer />
